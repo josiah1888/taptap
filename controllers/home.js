366071cc7f91ec2b22357e3d05d7e0db6d268a10
Josiah Burchard <josiah.burchard@gmail.com>
2015-01-24 15:22:16 -0600

    Restructured app. Added demo visualization.

---
+++ controllers/home.js
@@ -0,0 +1,13 @@
+﻿(function() {
+    'use strict';
+
+    var app = angular.module('taptap');
+
+    app.controller('Home', [function() {
+        var vm = this;
+
+        var version = '0.2';
+        vm.title = 'Tap Tap ' + version;
+
+    }]);
+})();
