e62e2b5bbe9851457c9ef31f9d4748e072a3ff83
Josiah Burchard <josiah.burchard@gmail.com>
2015-01-24 11:13:55 -0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,26 @@
+﻿(function() {
+    'use strict';
+
+    var app = angular.module('taptap');
+
+    app.config(function($routeProvider) {
+        $routeProvider.when('/',
+            {
+                templateUrl: 'templates/home.html',
+                controller: 'Home',
+                controllerAs: 'home'
+            });
+        $routeProvider.when('/demo',
+            {
+                templateUrl: 'templates/demo.html',
+                controller: 'Demo',
+                controllerAs: 'demo'
+            });
+        $routeProvider.otherwise(
+            {
+                redirectTo: '/'
+            });
+    });
+})();
+
+
